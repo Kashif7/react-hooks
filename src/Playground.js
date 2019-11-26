1fbebc1a1336da46c3db6dc5e2dddb343fb9a1aa
Kashif Roshen <kashif@wisenet.co>
2019-11-26 21:28:34 +0530

    playground component class refactored to use useState hook

---
+++ src/Playground.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import './Playground.css';
+
+export default function Playground() {
+    const [
+        text,
+        setText
+    ] = useState("");
+
+    const [
+        checked,
+        setChecked
+    ] = useState(false);
+  
+    const handleCheckboxToggle = () => setChecked(prevChecked => !prevChecked);
+    
+    return (
+      <section>
+        <input
+          type="text"
+          value={text}
+          onChange={e => setText( e.target.value)}
+        />
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={handleCheckboxToggle}
+        />
+        <ul>
+          <li>{text}</li>
+          <li>{checked.toString()}</li>
+        </ul>
+      </section>
+    );
+}
